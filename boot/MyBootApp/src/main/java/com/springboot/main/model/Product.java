3d3298a2cc7ee56f0c9a2084b3ec0a8aa4bda79b
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-06-20 13:28:28 +0530

    level 1 apis

---
+++ boot/MyBootApp/src/main/java/com/springboot/main/model/Product.java
@@ -0,0 +1,69 @@
+package com.springboot.main.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(length = 1500)
+	private String description;
+	
+	private double price;
+	
+	@ManyToOne
+	private Vendor vendor;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Vendor getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(Vendor vendor) {
+		this.vendor = vendor;
+	}
+	
+	
+}
